021d199d9174a915c2b5d9562780b5d08ce36e35
Voidaerun <francois.petit88@gmail.com>
2019-04-26 19:50:27 -0400

    working input test scene

---
+++ UnityProject/Assets/Scripts/InputTest.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Experimental.Input;
+
+public class InputTest : MonoBehaviour, GamepadControls.ITestActions
+{
+    public GamepadControls Controls;
+
+    // Start is called before the first frame update
+    void Awake()
+    {
+        Controls = new GamepadControls();
+        Controls.Test.SetCallbacks(this);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void OnEnable()
+    {
+        Controls.Test.Enable();
+    }
+
+    public void OnDisable()
+    {
+        Controls.Test.Disable();
+    }
+
+    public void OnMove(InputAction.CallbackContext context)
+    {
+        Vector3 input = context.ReadValue<Vector2>();
+
+        this.transform.position += input * Time.deltaTime;
+    }
+}
