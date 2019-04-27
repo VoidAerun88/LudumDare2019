573eb6c1e095aa3bdbb197aea59cca78f6f75a9c
Voidaerun <francois.petit88@gmail.com>
2019-04-27 12:39:06 -0400

    Remove input + add text data

---
+++ UnityProject/Assets/Scripts/TextSequence.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(menuName = "Create Text Sequence")]
+public class TextSequence : ScriptableObject
+{
+    [Serializable]
+    public class Dialog
+    {
+        public float TimeCondition;
+        public Color MessageColor = Color.white;
+        public string Sender;
+        public string SenderMessage;
+        public string CorrectResponse;
+        public List<string> WrongResponses;
+    }
+
+    public List<Dialog> DialogList;
+}
