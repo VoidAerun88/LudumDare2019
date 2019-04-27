cc81183c6f21a8356b1aee229a486ce584865e9d
jonathan_filion_ <filion.jonathan@gmail.com>
2019-04-27 14:22:27 -0400

    Advance message hub

--- UnityProject/Assets/Scripts/MessageHub.cs
+++ UnityProject/Assets/Scripts/MessageHub.cs
@@ -6,20 +6,38 @@ public class MessageHub : MonoBehaviour
 {
     public TextSequence TextSequence;
     public MessageBox MessageBox;
+    public float MinimumDelay = 5f;
 
     private int _ignoredCount;
-    private int _currentDialog;
+    private int _nextDialogIdx;
 
-    // Start is called before the first frame update
-    void Start()
+    private void Awake()
     {
-        var current = TextSequence.DialogList[_currentDialog];
-        //current
+        _nextDialogIdx = 0;
+        _ignoredCount = 0;
     }
 
-    // Update is called once per frame
+    // Start is called before the first frame update
     void Update()
     {
-        
+        if(_ignoredCount > 0)
+        {
+            StartCoroutine(WaitIgnoreTest());
+        }
+        else if(_nextDialogIdx < TextSequence.DialogList.Count)
+        {
+            var next = TextSequence.DialogList[_nextDialogIdx];
+            if(PhoneTime.Time >= next.TimeCondition)
+            {
+                //MessageBox.ShowMessage();
+                _nextDialogIdx++;
+            }
+        }
+    }
+
+    private IEnumerator WaitIgnoreTest()
+    {
+        yield return new WaitForSeconds(3f);
+        _ignoredCount--;
     }
 }
