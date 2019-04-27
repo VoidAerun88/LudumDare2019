790bbd10d1fc3febad12c1b885f5d2d347641c49
Voidaerun <francois.petit88@gmail.com>
2019-04-27 14:20:13 -0400

    First path message box layout

--- UnityProject/Assets/Scripts/MessageBox.cs
+++ UnityProject/Assets/Scripts/MessageBox.cs
@@ -1,18 +1,14 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
 
 public class MessageBox : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    public Image Photo;
+    public TMP_Text Name;
+    public TMP_Text Content;
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+
 }
