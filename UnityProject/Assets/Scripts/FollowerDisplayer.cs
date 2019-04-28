24bbc253f05107f3c160fd3e948e77efe7990d6f
Voidaerun <francois.petit88@gmail.com>
2019-04-28 09:18:59 -0400

    Ending scenes

---
+++ UnityProject/Assets/Scripts/FollowerDisplayer.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+public class FollowerDisplayer : MonoBehaviour
+{
+    public TMP_Text Display;
+    private string _originalText;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        _originalText = Display.text;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Display.text = string.Format(_originalText, FollowersManager.Followers);
+    }
+}
