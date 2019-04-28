fae03f85bc0b9375314a40cb6a599f4d8fddce7e
jonathan_filion_ <filion.jonathan@gmail.com>
2019-04-27 23:08:13 -0400

    timed event and fix BG

---
+++ UnityProject/Assets/Scripts/TimedEventsManager.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class TimedEventsManager : MonoBehaviour
+{
+    [System.Serializable]
+    public class TimedEvent
+    {
+        public float Time;
+        public UnityEvent Event;
+    }
+
+    public List<TimedEvent> TimedEvents;
+    private int _currentIdx;
+
+    private void Awake()
+    {
+        TimedEvents.Sort((x, y) => { return (x.Time.CompareTo(y.Time)); });
+    }
+
+    private void Update()
+    {
+        if(_currentIdx < TimedEvents.Count && PhoneTime.Time >= TimedEvents[_currentIdx].Time)
+        {
+            TimedEvents[_currentIdx].Event.Invoke() ;
+            _currentIdx++;
+        }
+    }
+}
